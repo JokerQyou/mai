2cb76245d773826bc58100f9934c0a1ce2f9d7af
Joker Qyou <Joker.Qyou@gmail.com>
2016-05-16 22:06:36 +0800

    Update shell context

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 from app import create_app, db
+from app.models import Role, User
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -19,7 +20,7 @@ def test():
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Role=Role, User=User)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
